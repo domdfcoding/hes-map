6e119b2a7266388646d6ead24689e180ac68b62f
domdfcoding <dominic@davis-foster.co.uk>
2026-08-17 18:47:18 +0000

    Deploying to gh-pages from @ domdfcoding/hes-map@397b500a2979e7b1b6b74dc828b1d206af7940f3 🚀

---
+++ data/nhle_3388854078.js
@@ -0,0 +1,25 @@
+// Lat: 59 — -5, Lng: 60 — -4,
+// Lat,Lng,Number,Name,Grade,ListDate,Link,Notes?, Polygon Points?,
+var battlefields3388854078 = [
+]
+
+// Lat,Lng,Number,Name,Grade,ListDate,Link,Notes?, Polygon Points?,
+var listedBuildings3388854078 = [
+[59.0843318899, -4.4065539237, "LB18598", "SULE SKERRY LIGHTHOUSE", "A", "Wed, 08 Dec 1971", "https://portal.historicenvironment.scot/designation/LB18598"],
+]
+
+// Lat,Lng,Number,Name,Grade,ListDate,Link,Notes?, Polygon Points?,
+var gardensLandscapes3388854078 = [
+]
+
+// Lat,Lng,Number,Name,Grade,ListDate,Link,Notes?, Polygon Points?,
+var marineProtectedAreas3388854078 = [
+]
+
+// Lat,Lng,Number,Name,Grade,ListDate,Link,Notes?, Polygon Points?,
+var scheduledMonuments3388854078 = [
+]
+
+// Lat,Lng,Number,Name,Grade,ListDate,Link,Notes?, Polygon Points?,
+var worldHeritageSites3388854078 = [
+]
